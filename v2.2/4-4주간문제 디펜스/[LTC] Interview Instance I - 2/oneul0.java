d7be0bca970fbe9b6b252b4d0fa1d9df4a56a068
oneul0 <neul.dev@gmail.com>
2026-04-26 14:51:29 +0900

    필수문제 9문제

--- v2.2/4-4주간문제 디펜스/[LTC] Interview Instance I - 2/oneul0.java
+++ v2.2/4-4주간문제 디펜스/[LTC] Interview Instance I - 2/oneul0.java
@@ -1,8 +1,25 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(a[1], b[1]);
+            }
+            return Integer.compare(a[0], b[0]);
+        });
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<int[]> list = new ArrayList<>();
+        list.add(intervals[0]);
+
+        for (int i = 1; i < intervals.length; i++) {
+            int[] last = list.get(list.size() - 1);
+
+            if (last[1] < intervals[i][0]) {
+                list.add(intervals[i]);
+            } else {
+                last[1] = Math.max(last[1], intervals[i][1]);
+            }
+        }
+
+        return list.toArray(new int[list.size()][]);
     }
 }
--- v2.2/4-4주간문제 디펜스/[LTC] Interview Instance III - 3/oneul0.java
+++ v2.2/4-4주간문제 디펜스/[LTC] Interview Instance III - 3/oneul0.java
@@ -1,8 +1,26 @@
-import java.io.*;
-import java.util.*;
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        int n = heights.length;
+        int[] answer = new int[n];
+        Deque<Integer> stack = new ArrayDeque<>();
 
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = n-1; i >= 0; i--) {
+            int count = 0;
+
+            //만난거보다 작으면 다 빼버림
+            while (!stack.isEmpty() && stack.peek() < heights[i]) {
+                stack.pop();
+                count++;
+            }
+
+            if (!stack.isEmpty()) {
+                count++;
+            }
+
+            answer[i] = count;
+            stack.push(heights[i]);
+        }
+
+        return answer;
     }
 }

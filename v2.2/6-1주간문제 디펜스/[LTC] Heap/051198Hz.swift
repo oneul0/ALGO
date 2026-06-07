f15f6a3553684ac5eb116c4207f31f7c226133a7
051198Hz <peeper_o_o@naver.com>
2026-06-07 16:45:38 +0900

    필수 6챕터 중 6챕터 + 오답풀이

--- v2.2/6-1주간문제 디펜스/[LTC] Heap/051198Hz.swift
+++ v2.2/6-1주간문제 디펜스/[LTC] Heap/051198Hz.swift
@@ -1,3 +1,70 @@
-import Foundation
+class Solution {
+    func lastStoneWeight(_ stones: [Int]) -> Int {
+        var heap = Heap<Int>()
+        heap.insert(contentsOf: stones)
+        
+        while true {
+            guard let a = heap.popMax() else { return 0 }
+            guard let b = heap.popMax() else { return a }
+            heap.insert(abs(a-b))
+        }
 
-// \(username)
+        return heap.max ?? 0
+    }   
+
+    struct SumPair: Comparable {
+        let sum: Int
+        let i: Int
+        let j: Int
+
+        static func < (lhs: SumPair, rhs: SumPair) -> Bool {
+            return lhs.sum < rhs.sum
+        }
+    }
+
+    func kSmallestPairs(_ nums1: [Int], _ nums2: [Int], _ k: Int) -> [[Int]] {
+        var pairs = [[Int]]()
+        var heap = Heap<SumPair>()
+        (0..<nums1.count).forEach {
+            heap.insert(SumPair(sum: nums1[$0] + nums2[0], i: $0, j: 0))
+        }
+
+        while pairs.count < k {
+            guard let minSum = heap.popMin() else { return [[]] }
+
+            let a = nums1[minSum.i]
+            let b = nums2[minSum.j]
+
+            pairs.append([a, b])
+            guard minSum.j + 1 < nums2.count else { continue }
+            heap.insert(SumPair(sum: nums1[minSum.i] + nums2[minSum.j + 1], i: minSum.i, j: minSum.j + 1))
+        }
+
+        return pairs
+    }
+    
+    func isPossible(_ target: [Int]) -> Bool {
+        var sum = target.reduce(0, +)
+        var heap = Heap<Int>()
+        heap.insert(contentsOf: target)
+        
+        while let largest = heap.popMax() {
+            let rest = sum - largest
+
+            if rest == 1 { return true }
+            if largest == 1 { return true }
+
+            guard rest < largest else { return false }
+            guard rest != 0 else { return false }
+
+            let prev = largest % rest
+
+            if prev == 0 { return false }
+
+            sum = rest + prev
+            heap.insert(prev)
+        }
+        
+        return false
+    }
+}
--- v2.2/6-1주간문제 디펜스/[LTC] Queue/051198Hz.swift
+++ v2.2/6-1주간문제 디펜스/[LTC] Queue/051198Hz.swift
@@ -1,3 +1,93 @@
-import Foundation
+class Solution {
+    func countStudents(_ students: [Int], _ sandwiches: [Int]) -> Int {
+        var stack = sandwiches
+        var stackHead = 0
 
-// \(username)
+        var queue = students
+        var queueHead = 0
+
+        var unableEaterCount = 0
+
+        while queue.count > queueHead {
+            if queue.count - queueHead == unableEaterCount {
+                break
+            }
+            if stack[stackHead] == queue[queueHead] {
+                unableEaterCount = 0
+                queueHead += 1
+                stackHead += 1
+                continue
+            }
+            unableEaterCount += 1
+            queue.append(queue[queueHead])
+            queueHead += 1
+        }
+        
+        return unableEaterCount
+    }
+
+    func timeRequiredToBuy(_ tickets: [Int], _ k: Int) -> Int {
+        var queue = tickets
+        let ticketAmount = tickets.count
+        var cycleIndex = -1
+        var timeTaken = 0
+        var head = 0
+        
+        while true {
+            let currentTikets = queue[head] - 1
+
+            cycleIndex += 1
+            cycleIndex %= ticketAmount
+
+            if cycleIndex == k, currentTikets == 0 {
+                timeTaken += 1
+                break
+            }
+            if currentTikets >= 0 {
+                timeTaken += 1
+            }
+            queue.append(max(currentTikets, 0))
+            head += 1
+        }
+
+        return timeTaken
+    }
+}
+
+class MyQueue {
+
+    private var outQueue = [Int]()
+    private var inQueue = [Int]()
+
+    init() {
+        
+    }
+    
+    func push(_ x: Int) {
+        inQueue.append(x)
+    }
+    
+    func pop() -> Int {
+        if outQueue.isEmpty {
+            outQueue = reverse()
+        }
+        return outQueue.removeLast()
+    }
+    
+    func peek() -> Int {
+        guard let peek = outQueue.last else { return inQueue.first ?? 0 }
+        return peek
+    }
+    
+    func empty() -> Bool {
+        return inQueue.isEmpty && outQueue.isEmpty
+    }
+
+    private func reverse() -> [Int] {
+        var stack = [Int]()
+        while !inQueue.isEmpty {
+            stack.append(inQueue.removeLast())
+        }
+        return stack
+    }
+}

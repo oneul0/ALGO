6fb4affc02fb65c5262816082eee365ef7174983
051198Hz <peeper_o_o@naver.com>
2026-06-14 10:40:16 +0900

    solve: quizes

--- v2.2/6-2주간문제 디펜스/[LTC] Sequence Valley Quiz 1/051198Hz.swift
+++ v2.2/6-2주간문제 디펜스/[LTC] Sequence Valley Quiz 1/051198Hz.swift
@@ -1,3 +1,109 @@
-import Foundation
+import Collections
 
-// \(username)
+struct Apple: Comparable {
+    let expire: Int
+    var count: Int
+
+    static func < (lhs: Apple, rhs: Apple) -> Bool {
+        lhs.expire < rhs.expire
+    }
+}
+
+class Solution {
+    func eatenApples(_ apples: [Int], _ days: [Int]) -> Int {
+        var heap = Heap<Apple>()
+
+        var day = 0
+        var answer = 0
+
+        while day < apples.count || !heap.isEmpty {
+
+            if day < apples.count, apples[day] > 0 {
+                heap.insert(
+                    Apple(
+                        expire: day + days[day],
+                        count: apples[day]
+                    )
+                )
+            }
+
+            while let apple = heap.min,
+                  apple.expire <= day {
+                heap.removeMin()
+            }
+
+            if var apple = heap.popMin() {
+                answer += 1
+
+                apple.count -= 1
+
+                if apple.count > 0 {
+                    heap.insert(apple)
+                }
+            }
+
+            day += 1
+        }
+
+        return answer
+    }
+}
+
+class MyCircularQueue {
+
+    private let capacity: Int
+    private var queue: [Int]
+
+    private var head = 0
+    private var tail = 0
+    private var count = 0
+
+    init(_ k: Int) {
+        capacity = k
+        queue = Array(repeating: 0, count: k)
+    }
+
+    func enQueue(_ value: Int) -> Bool {
+        guard !isFull() else {
+            return false
+        }
+
+        queue[tail] = value
+        tail = (tail + 1) % capacity
+        count += 1
+
+        return true
+    }
+
+    func deQueue() -> Bool {
+        guard !isEmpty() else {
+            return false
+        }
+
+        head = (head + 1) % capacity
+        count -= 1
+
+        return true
+    }
+
+    func Front() -> Int {
+        isEmpty() ? -1 : queue[head]
+    }
+
+    func Rear() -> Int {
+        guard !isEmpty() else {
+            return -1
+        }
+
+        let index = (tail - 1 + capacity) % capacity
+        return queue[index]
+    }
+
+    func isEmpty() -> Bool {
+        count == 0
+    }
+
+    func isFull() -> Bool {
+        count == capacity
+    }
+}
